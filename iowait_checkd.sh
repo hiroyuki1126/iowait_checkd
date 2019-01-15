fad54b027241fc5ed58a02e3e4047ec157638a3c
Hiroyuki Ishihara <hiroyuki.1126@icloud.com>
2019-01-15 13:01:22 +0900

    軽微な修正。

--- iowait_checkd.sh
+++ iowait_checkd.sh
@@ -1,39 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 . /etc/rc.d/init.d/functions
 
-PROG=/root/bin/iowait_checkd
-PROGNAME=`basename ${PROG}`
-CONF=/root/conf/iowait_checkd.conf
+readonly PROG=/root/bin/iowait_checkd
+readonly PROGNAME=$(basename ${PROG})
+readonly CONF=/root/conf/iowait_checkd.conf
 
-error() {
-    echo ERROR: `basename $0`: $@ 1>&2
+function error() {
+    echo ERROR: $(basename $0): $@ 1>&2
 }
 
-abort() {
+function abort() {
     error $@
     exit 1
 }
 
-start() {
+function start() {
     echo -n "Starting ${PROGNAME}: "
     daemon ${PROG} ${CONF}
     echo
 }
 
-stop() {
+function stop() {
     echo -n "Stopping ${PROGNAME}: "
     killproc ${PROGNAME}
     echo
 }
 
-reload() {
+function reload() {
     echo -n "Reloading ${PROGNAME}: "
     killproc ${PROGNAME} -HUP
     echo
 }
 
-[ `whoami` == 'root' ] || abort 'This program must be run as root.'
+[ $(whoami) == 'root' ] || abort 'This program must be run as root.'
 
 [ -f ${PROG} ] || abort "Program '${PROG}' does not exist."
 
@@ -55,7 +55,7 @@ case "$1" in
         status ${PROGNAME}
         ;;
     *)
-        abort "Usage: `basename $0` {start|stop|restart|reload|status}"
+        abort "Usage: $(basename $0) {start|stop|restart|reload|status}"
         ;;
 esac
 
